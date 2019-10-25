7d653f0851edf7849f5f073dba577848114ba278
Hevesi István <hevesiistvan71@hotmail.com>
2019-10-25 12:07:26 +0200

    gyakorol kész

--- 10_21_felhsznnev_eletkor/adatbazis/gyakorol.php
+++ 10_21_felhsznnev_eletkor/adatbazis/gyakorol.php
@@ -13,3 +13,17 @@ if ($connect -> errno) {
 
 $connect -> set_charset('utf8');
 
+$sql = "SELECT * FROM torpek";
+$result = $connect -> query($sql);
+
+if(!$result){
+    echo("Hiba a lekérdezésben");
+}else {
+    while ($row = $result -> fetch_assoc()) {
+        echo $row['id'].", ";
+        echo $row['nev'].", ";
+        echo $row['klan'].", ";
+        echo $row['nem'].", ";
+        echo $row['magassag']."<br>";
+    }
+}
