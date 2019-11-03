d7ffbe4e801d2616081d91661a915e4a6d0b1226
Hevesi István <hevesiistvan71@hotmail.com>
2019-11-03 14:25:25 +0100

    php klónozva

--- 10_21_felhsznnev_eletkor/adatbazis/gyakorol.php
+++ 10_21_felhsznnev_eletkor/adatbazis/gyakorol.php
@@ -1,24 +1,29 @@
 <?php
-
 $host = "localhost";
-$dbUser  = "root";
-$dbPwd  = "";
-$dbName  = "torpetarna";
+$dbUser = "root";
+$dbPwd = "";
+$dbName = "torpetarna";
 
-$kapcsolat = new mysqli($host, $dbUser, $dbPwd, $dbName);
+$connect = new mysqli($host, $dbUser, $dbPwd, $dbName);
 
-$kiolvas = "SELECT * FROM torpek";
-$kiolvasottadatok = $kapcsolat -> query($kiolvas);
-if (!$kiolvasottadatok) {
-    echo("hiba");
-} else {
-    while ($row = $kiolvasottadatok -> fetch_assoc()) {
-    //id    nev     klan    nem     suly    magassag
-        echo $row['id'].", ";
-        echo $row['nev'].", ";
-        echo $row['klan'].", ";
-        echo $row['nem'].", ";
-        echo $row['suly'].", ";
-        echo $row['magassag'].", ";
-    }
+if ($connect-> errno) {
+    die("Hiba a csatlakozás során");
 }
+
+$connect -> set_charset('utf8');
+
+$sql = "SELECT * FROM torpek";
+$result = $connect -> query($sql);
+if (!$result) {
+    echo("hiba a lekérdezés során");
+} else{
+    //id    nev     klan    nem     suly    magassag
+    echo $row['id'].", ";
+    echo $row['nev'].", ";
+    echo $row['klan'].", ";
+    echo $row['nem'].", ";
+    echo $row['suly'].", ";
+    echo $row['magassag']."<br>";
+    
+}
+
